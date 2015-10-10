c8525f0c2bde2ce6b9ed039ed7d0abfc75e2c507
Ashley Coleman <ashley@yieldify.com>
2015-10-10 17:59:49 +0100

    update: Task 3

--- program.js
+++ program.js
@@ -1,4 +1,6 @@
-var sum = process.argv.slice(2).reduce(function (initial, next, index, arr) {
-    return Number(initial || 0) + Number(next);
-});
-console.log(sum);
+
+var fs = require('fs');
+var filePath = process.argv[2]
+var content = fs.readFileSync(filePath, {encoding: 'utf8'});
+
+console.log(content.split('\n').length - 1);
