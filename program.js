e6fab4ce2bbbcf5b42c7a6a957ffae99f94a6e52
Ashley Coleman <ashley@yieldify.com>
2015-10-10 18:09:39 +0100

    update: Task 4

--- program.js
+++ program.js
@@ -1,6 +1,22 @@
 
 var fs = require('fs');
-var filePath = process.argv[2]
-var content = fs.readFileSync(filePath, {encoding: 'utf8'});
+var filePath = process.argv[2];
+var filter = process.argv[3];
 
-console.log(content.split('\n').length - 1);
+fs.readdir(filePath, function (error, content) {
+    content.forEach( function (element, index, array) {lea
+        if (element.endsWith('.'+filter)) {
+            console.log(element);
+        }
+    });
+});
+
+    // var fs = require('fs')
+    // var path = require('path')
+
+    // fs.readdir(process.argv[2], function (err, list) {
+    //   list.forEach(function (file) {
+    //     if (path.extname(file) === '.' + process.argv[3])
+    //       console.log(file)
+    //   })
+    // })
