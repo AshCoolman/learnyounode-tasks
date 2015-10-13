e72ed12a97f42610447b5916a14b9ee5e27ac922
Ashley Coleman <ashley@yieldify.com>
2015-10-13 05:34:55 +0100

    update: task 8

--- program.js
+++ program.js
@@ -1,12 +1,18 @@
-var http = require('http');
+var bl = require('bl'),
+    http = require('http');
+
 var nodeInit = process.argv[0],
     file = process.argv[1],
     url = process.argv[2] || 'http://www.google.com';
 
-var client = http.get(url, function (request, reponse) {
+var client = http.get(url, function (request) {
     request.setEncoding('utf8');
-    request.on('error', console.error);
-    request.on('data', console.log);
+    request.pipe(bl(function (err, data) {
+        console.log(data.length);
+        console.log(data.toString());
+    }));
+    // request.on('error', console.error);
+    // request.on('data', console.log);
 });
 
 // console.log('client:', client)
