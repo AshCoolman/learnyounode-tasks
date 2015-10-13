7e6475bc8c640c273e2ff609b34de67f00ca5986
Ashley Coleman <ashley@yieldify.com>
2015-10-13 05:51:49 +0100

    update: task 10

--- program.js
+++ program.js
@@ -1,23 +1,13 @@
-var bl = require('bl'),
-    http = require('http');
+var port = process.argv[2];
 
-var nodeInit = process.argv[0],
-    file = process.argv[1],
-    urls = process.argv.splice(2, 3).reverse();
+var getTime = function () {
+    return require('moment')().format("YYYY-MM-DD hh:mm");
+}
 
-
-
-var printGet = function (url) {
-    if (url) {
-        http.get(url, function (request) {
-            request.setEncoding('utf8');
-            request.pipe(bl(function (err, data) {
-                console.log(data.toString());
-            }));
-            request.on('end', function () {
-                printGet(urls.pop());
-            })
-        });
-    }
-};
-printGet(urls.pop());
+var server = require('net')
+    .createServer(
+        function (socket) {
+            socket.end(getTime());
+        }
+    )
+    .listen(port);
