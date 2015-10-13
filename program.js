f839ebd9af119c5dd28853530471a6768d334a8b
Ashley Coleman <ashley@yieldify.com>
2015-10-13 05:41:25 +0100

    update: Task 9

--- program.js
+++ program.js
@@ -3,16 +3,21 @@ var bl = require('bl'),
 
 var nodeInit = process.argv[0],
     file = process.argv[1],
-    url = process.argv[2] || 'http://www.google.com';
+    urls = process.argv.splice(2, 3).reverse();
 
-var client = http.get(url, function (request) {
-    request.setEncoding('utf8');
-    request.pipe(bl(function (err, data) {
-        console.log(data.length);
-        console.log(data.toString());
-    }));
-    // request.on('error', console.error);
-    // request.on('data', console.log);
-});
 
-// console.log('client:', client)
+
+var printGet = function (url) {
+    if (url) {
+        http.get(url, function (request) {
+            request.setEncoding('utf8');
+            request.pipe(bl(function (err, data) {
+                console.log(data.toString());
+            }));
+            request.on('end', function () {
+                printGet(urls.pop());
+            })
+        });
+    }
+};
+printGet(urls.pop());
